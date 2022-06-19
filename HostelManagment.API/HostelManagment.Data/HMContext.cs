2e67b366fb1be6adbb61e669394f6df2643b4336
Shalu Rawat <57206569+shalurawat-sr@users.noreply.github.com>
2022-06-20 01:18:26 +0530

    Add files via upload

---
+++ HostelManagment.API/HostelManagment.Data/HMContext.cs
@@ -0,0 +1,21 @@
+﻿using HostelManagment.Models.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostelManagment.Data
+{
+    public class HMContext : DbContext
+    {
+        public HMContext() : base("HMDBConnectionString")
+        {
+        }
+        public DbSet<Admins> Admins { get; set; }
+        public DbSet<Students> Students { get; set; }
+        public DbSet<Rooms> Rooms { get; set; }
+        public DbSet<RoomAllocation> RoomAllocation { get; set; }
+    }
+}
