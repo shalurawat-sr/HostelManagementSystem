c7fd3c592b664bf6defad9aac2644705087d940c
Shalu Rawat <57206569+shalurawat-sr@users.noreply.github.com>
2022-06-20 00:49:15 +0530

    Add files via upload

---
+++ HostelManagment.API/HostelManagment.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using HostelManagement.API.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace HostelManagment.API
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+
+            config.EnableCors(cors);
+
+            config.MapHttpAttributeRoutes();
+
+            config.MessageHandlers.Add(new TokenValidationHandler());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
